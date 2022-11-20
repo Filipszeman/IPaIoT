49572ae285185af22d59de052f762f9bb2cc02a9
Filipszeman <114649866+Filipszeman@users.noreply.github.com>
2022-11-20 19:26:51 +0100

    create

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
-<?php 
-echo '<h1>First web on Azure</p>';
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset='utf-8'>
+        <title>PHP web form</title>
+    </head>
+    <body>
+        <main>
+            <p>PHP web form - Szeman Filip</p>
+            <form class="contact-form" action="contactform.php" method="post">
+                <input type="text" name="First name" placeholder="First name">
+                <input type="text" name="Last name" placeholder="Last name">
+                <input type="text" name="Age" placeholder="Age">
+                <input type="text" name="Email" placeholder="Email">
+                <input type="text" name="Country" placeholder="Country">
+                <input type="text" name="City" placeholder="City">
+                <button type="Submit" name="Submit">Submit form</button>
+        </main>
+    </body>
+</html>
