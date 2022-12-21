96390fb8b6e41f29cb94ee8a90343dd436fa6f2e
Filip Szeman <114649866+Filipszeman@users.noreply.github.com>
2022-12-21 18:08:04 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
     echo '<h1>ESP hodnoty</h1>';
         
+    $test = 1;
     $motion = $_GET["a"];
     $light = $_GET["b"];
     $fire = $_GET["c"];
@@ -10,8 +11,11 @@
     $text1 = "Motion was detected ataa " . $date;
     $text2 = "Light was detected at " . $date;
     $text3 = "Fire was detected at " . $date;
-    $fp = fopen("data.txt",'a');
+    $fp = fopen("data.txt",'w');
     
+    if($test == 1){
+        fwrite($fp, $text3);
+    }
     
     if($motion == 1){  
         fwrite($fp, $text1);
