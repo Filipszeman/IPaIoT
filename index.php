ee7ad192055d7e94f099da54db7fcd77f805c804
Filip Szeman <114649866+Filipszeman@users.noreply.github.com>
2022-12-21 18:23:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -14,9 +14,9 @@
     $fp = fopen("data.txt",'w');
     
     if($test == 1){
-        fwrite($fp, 'Motion was detected at' . $date);
+        fwrite($fp, 'Motion was detected at ' . $date);
     }
-    fwrite($fp, 'Fire was detected at' . $date);
+    fwrite($fp, 'Fire was detected at ' . $date);
     
     if($motion == 1){  
         fwrite($fp, $text1);
