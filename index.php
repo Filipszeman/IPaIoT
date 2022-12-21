d70521013dfa208f0a3ce8c41ba82f3779b6ce5e
Filip Szeman <114649866+Filipszeman@users.noreply.github.com>
2022-12-21 19:08:56 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,9 +7,6 @@
     date_default_timezone_set('Europe/Bratislava');
     $date = date('Y-m-d H:i:s');
 
-    $text1 = "Motion was detected ataa " . $date;
-    $text2 = "Light was detected at " . $date;
-    $text3 = "Fire was detected at " . $date;
     $fp = fopen("data.txt",'a');
     
     if($motion == 1){  
