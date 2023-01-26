7d375824ac957af7b141fce0df5579244db50f35
Filip Szeman <114649866+Filipszeman@users.noreply.github.com>
2023-01-26 09:43:04 +0100

    create

--- index.php
+++ index.php
@@ -1,112 +1,14 @@
-<html>
-    <head>
-        <meta charset='utf-8'>
-    </head>
-    <body>
-        <main>
-            <form class="contact-form" method="post" accept-charset="UTF-8">
-                <input type="number" name="first_number" placeholder="First number">
-                <input type="number" name="second_number" placeholder="Second number">
-                <input type="number" name="third_number" placeholder="Third number">
-                <input type="text" name="first_operator" placeholder="First operator">
-                <input type="text" name="second_operator" placeholder="Second operator">
-                <button type="Submit" name="Submit">Submit form</button>
-        </main>
-    </body>
+<html>  
+   <body>  
+     
+      <form action = "get_data.php" method = "get">  
+         First number: <input type = "number" name = "first_number" /> <br>  
+         Second number: <input type = "number" name = "second_number" /> <br>  
+         Third number: <input type = "number" name = "third_number" /> <br>
+         First operator (only + - * /): <input type = "text" name = "first_operator" /> <br>  
+         Second operator (only + - * /): <input type = "text" name = "second_operator" /> <br>  
+         <input type = "submit" />  
+      </form>  
+        
+   </body>  
 </html>
-
-<?php   
-    $first_number = $_POST["first_number"];
-    $second_number = $_POST["second_number"];
-    $third_number = $_POST["third_number"];
-    $first_operator = $_POST["first_operator"];
-    $second_operator = $_POST["second_operator"];
-        
-    $text1 = "First number = " . $first_number . " Second number = " . $second_number . " Third number = " . $third_number;
-    $text2 = "As first operator you chose : " . $first_operator . " and as second operator you chose : " . $second_operator;
-
-    if($first_operator == '+'){  
-        if($second_operator == '+'){  
-            $result = $first_number + $second_number + $third_number;
-        }  
-        elseif($second_operator == '-'){  
-            $result = $first_number + $second_number - $third_number;
-        }  
-        elseif($second_operator == '*'){  
-            $result = $first_number + ($second_number * $third_number);
-        }  
-        elseif($second_operator == '-'){  
-            $result = $first_number + ($second_number / $third_number);
-        }
-        else {
-            $result = "Error, the second operator is not correct."
-        }
-    }  
-
-    elseif($first_operator == '-'){  
-        if($second_operator == '+'){  
-            $result = $first_number - $second_number + $third_number;
-        }  
-        elseif($second_operator == '-'){  
-            $result = $first_number - $second_number - $third_number;
-        }  
-        elseif($second_operator == '*'){  
-            $result = $first_number - ($second_number * $third_number);
-        }  
-        elseif($second_operator == '-'){  
-            $result = $first_number - ($second_number / $third_number);
-        }  
-        else {
-            $result = "Error, the second operator is not correct."
-        }
-    }
-
-    elseif($first_operator == '*'){  
-        if($second_operator == '+'){  
-            $result = $first_number * $second_number + $third_number;
-        }  
-        elseif($second_operator == '-'){  
-            $result = $first_number * $second_number - $third_number;
-        }  
-        elseif($second_operator == '*'){  
-            $result = $first_number * $second_number * $third_number;
-        }  
-        elseif($second_operator == '-'){  
-            $result = $first_number * $second_number / $third_number;
-        }  
-        else {
-            $result = "Error, the second operator is not correct."
-        }
-    }
-
-    elseif($first_operator == '/'){  
-        if($second_operator == '+'){  
-            $result = $first_number / $second_number + $third_number;
-        }  
-        elseif($second_operator == '-'){  
-            $result = $first_number / $second_number - $third_number;
-        }  
-        elseif($second_operator == '*'){  
-            $result = $first_number / $second_number * $third_number;
-        }  
-        elseif($second_operator == '-'){  
-            $result = $first_number / $second_number / $third_number;
-        }  
-        else {
-            $result = "Error, the second operator is not correct."
-        }
-    }
-
-    else {
-        $result = "Error, the first operator is not correct."
-    }
-
-    $the_result = "The result = " . $result;
-        
-    $fp = fopen("data.txt",'w');
-
-    fwrite($fp, $text1);
-    fwrite($fp, $text2);
-    fwrite($fp, $the_result);
-    fclose($fp);
-?>
